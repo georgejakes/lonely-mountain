51f147ec79253144610b177741b6ade6c1e4855e
Saagar Gugwad <saagar.gugwad@gmail.com>
2014-11-10 14:22:01 +0530

    Adding initial set of code.
    This code is generated using yeoman generator.

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('lmApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
