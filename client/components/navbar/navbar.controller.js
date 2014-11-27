2d2db2f5b01c1435e2550c29bd7e427514c91fba
Sagar Gugwad <saagar.gugwad@gmail.com>
2014-11-27 17:51:55 +0530

    Ui changes in main html.
    Changed the content in main html page.

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -3,7 +3,7 @@
 angular.module('lmApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
-      'title': 'Home',
+      'title': 'Search',
       'link': '/'
     }];
 
