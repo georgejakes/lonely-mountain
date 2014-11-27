2c181a06b504f53b05c076c3b8a20ae5a3789638
Sagar Gugwad <saagar.gugwad@gmail.com>
2014-11-27 16:02:52 +0530

    Added firefox and chrome in karma test runner.

--- karma.conf.js
+++ karma.conf.js
@@ -70,7 +70,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Firefox'],// ['PhantomJS'],
+    browsers: ['Firefox','Chrome'],// ['PhantomJS'],
 
 
     // Continuous Integration mode
